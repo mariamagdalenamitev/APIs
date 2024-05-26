6a3a7d75389ab4e6168010ee98acd62da09afbf0
mariamagdalena <mariamagdalenamitev@alumnat.iesdamiahuguet.net>
2024-05-26 11:52:35 +0200

    Commit casa

--- consultaApis/scriptEncanteris.js
+++ consultaApis/scriptEncanteris.js
@@ -17,3 +17,36 @@ async function getEncanteris() {
 
 //Crida getLlibres
 getEncanteris()
+
+const url='https://potterapi-fedeperin.vercel.app/es/spells'
+
+fetch(url)
+    .then(response=> response.json() )
+    .then(data => {
+
+        const table = document.getElementById("alicuotas");
+
+        data.forEach((e,i) => {    //< ---  recorremos data
+
+            let tr = document.createElement("tr"); //< ---  creamos una fila
+
+            let td = document.createElement("td"); //< ---  Hacemos columna index dentro de la fila
+            td.classList.add("index");
+            td.innerHTML = i;
+            tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+            for (p in e) {  //< ---  recorremos cada propiedad de cada elemento
+
+                let td = document.createElement("td"); //< ---  Hacemos columna dentro de la fila
+                td.classList.add(p);//<-- le podemos agregar a toda la columna la misma clase
+                td.innerHTML = e[p];
+
+                tr.appendChild(td); //< --- Agregamos la columna en la fila
+
+            }
+
+            table.appendChild(tr); //< --- Agregamos la fila a la tabla
+
+        });
+    })
+    .catch(err=>console.log(err))
